f0a25b9d6570be09767d6407b96ed521ab4e43f3
manda <manda@Ayogreen.com>
2025-10-09 09:27:44 +0000

    access to details and edit from list of owners

--- src/features/admin/module/owner/components/tables/owner/owner-table.tsx
+++ src/features/admin/module/owner/components/tables/owner/owner-table.tsx
@@ -1,8 +1,15 @@
 import { DataTable } from '@/shared/components/ui/data-table/data-table';
 import { columns, Owner } from './columns';
 
-const OwnerTable = ({ data }: { data: Owner[] }) => {
-    return <DataTable columns={columns} data={data} />;
+interface OwnerTableProps {
+  data: Owner[];
+  onDetails: (id: string) => void;
+  onEdit: (id: string) => void;
+  onDelete: (id: string) => Promise<void>;
+}
+
+const OwnerTable = ({ data, onDetails, onEdit, onDelete }: OwnerTableProps) => {
+  return <DataTable columns={columns(onDetails, onEdit, onDelete)} data={data} />;
 };
 
 export default OwnerTable;
