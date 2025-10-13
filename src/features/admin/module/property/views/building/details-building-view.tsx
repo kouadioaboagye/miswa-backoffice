438fb3f70b6d74f94e891e8ffe1fd03450b101a6
manda <manda@Ayogreen.com>
2025-10-13 22:35:54 +0000

    display building details

--- src/features/admin/module/property/views/building/details-building-view.tsx
+++ src/features/admin/module/property/views/building/details-building-view.tsx
@@ -12,9 +12,12 @@ import { useRouter } from "next/navigation";
 const BuildingDetailView = ({ idBuilding }: { idBuilding: string }) => {
     const router = useRouter()
     const { data, isLoading, error } = useGetBuildingByIdQuery(idBuilding)
+    const batiment = data?.batiment;
     if (error) {
         toast.error(error instanceof Error ? error.message : 'Une erreur est survenue.');
     }
+
+    console.log(data, "batiment")
 
     return (
         <div className="flex flex-col space-y-20">
@@ -51,7 +54,7 @@ const BuildingDetailView = ({ idBuilding }: { idBuilding: string }) => {
                 <div className="flex w-full space-x-4">
                     <div className="w-4/5">
                         <Image
-                            src={data?.cover_url || "/fallback-image.jpg"}
+                            src={batiment?.cover_url || "/fallback-image.jpg"}
                             width={100}
                             height={10}
                             alt="cover"
@@ -59,7 +62,7 @@ const BuildingDetailView = ({ idBuilding }: { idBuilding: string }) => {
                         />
                     </div>
                     <div className="w-1/5 space-y-4">
-                        {data?.photos && data?.photos.length > 0 && data.photos.slice(0, 2).map((photo: string, index: number) => (
+                        {batiment?.photos && batiment?.photos.length > 0 && batiment.photos.slice(0, 2).map((photo: string, index: number) => (
                             <Image
                                 key={index + 1}
                                 src={photo || ""}
@@ -72,11 +75,11 @@ const BuildingDetailView = ({ idBuilding }: { idBuilding: string }) => {
                     </div>
                 </div>
                 <div>
-                    <h2 className="text-[24px] font-bold mb-2">{data?.name}</h2>
+                    <h2 className="text-[24px] font-bold mb-2">{batiment?.name}</h2>
                     <div className="flex space-x-10">
                         <div className="flex items-center gap-2 text-[#778088]">
                             <MapPin className="w-5 h-5" />
-                            <span>{data?.address}</span>
+                            <span>{batiment?.address}</span>
                         </div>
                         <div className="flex items-center gap-2 text-[#778088]">
                             <Presentation className="w-5 h-5" />
@@ -87,13 +90,13 @@ const BuildingDetailView = ({ idBuilding }: { idBuilding: string }) => {
                 <div>
                     <h2 className="text-[24px] font-bold mb-2">Description</h2>
                     <p className="text-[#778088]">
-                        {data?.description}
+                        {batiment?.description}
                     </p>
                 </div>
             </div>
             <div>
                 <h2 className="text-[24px] font-bold mb-2">Appartements  du batiment</h2>
-                <ApartmentBuildingTable data={[]}/>
+                <ApartmentBuildingTable data={data?.proprietes || []}/>
             </div>
         </div>
     );
