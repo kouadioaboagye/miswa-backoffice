01e3a0708a475107cbf2ea334c0b7cb47c10ce30
manda <manda@Ayogreen.com>
2025-10-15 22:56:37 +0000

    contract dashboard ui

--- src/app/admin/module/contract/page.tsx
+++ src/app/admin/module/contract/page.tsx
@@ -1,11 +1,10 @@
+import DashboardContractView from '@/features/admin/module/contract/views/dashboard-view'
 import React from 'react'
 
 function page() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <DashboardContractView />
+    )
 }
 
 export default page
