c09c2349ba5c4737d36c5fae2a0e359f7d4557e7
manda <manda@Ayogreen.com>
2025-10-20 21:44:25 +0000

    add built_year in asset's details

--- src/features/admin/module/property/views/asset/details-asset-view.tsx
+++ src/features/admin/module/property/views/asset/details-asset-view.tsx
@@ -124,7 +124,7 @@ const DetailsAssetView = ({ idAsset }: { idAsset: string }) => {
                     </div>
                     <div>
                         <h2 className="text-[24px] font-bold mb-2">Caractéristiques</h2>
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                        <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
                             <div className="border border-gray-200 rounded-lg p-4 shadow-sm">
                                 <div className="flex items-start gap-3">
                                     <div>
@@ -146,6 +146,14 @@ const DetailsAssetView = ({ idAsset }: { idAsset: string }) => {
                                     <div>
                                         <h3 className="font-semibold text-gray-900 mb-1">Nombre de pièces</h3>
                                         <p className="text-lg text-gray-600">{asset?.rooms_count} pièces</p>
+                                    </div>
+                                </div>
+                            </div>
+                                                        <div className="border border-gray-200 rounded-lg p-4 shadow-sm">
+                                <div className="flex items-start gap-3">
+                                    <div>
+                                        <h3 className="font-semibold text-gray-900 mb-1">Année de construction</h3>
+                                        <p className="text-lg text-gray-600">{asset?.built_year}</p>
                                     </div>
                                 </div>
                             </div>
